7bac48afecb6d6543302e8141faee7fc67b43eb6
guilhermyfagundes-collab <guilhermy.fagundes@aluno.senai.br>
2026-02-11 14:19:59 -0300

    ola mundo pronto

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import flet as ft
 
 def main(page: ft.Page):
+    page.title = "Meu Primeiro App Flet"
+    page.bgcolor = "blue"
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     page.add(
         ft.Text("Guilhermy Henricky"),
         ft.ElevatedButton('Clique Aqui')
